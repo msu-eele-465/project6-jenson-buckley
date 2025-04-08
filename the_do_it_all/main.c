8f9990b0a8235cc7023ffb844c0463f615fb77fa
Peter Buckley <peterabbit22.22@gmail.com>
2025-04-08 11:55:13 -0600

    Added instantiation for lcd

--- the_do_it_all/main.c
+++ the_do_it_all/main.c
@@ -29,6 +29,20 @@ char lastKey = 'X';                         // used internally for debouncing
 //-- PELTIER GPIO CONTROL
 
 //-- LCD
+#define LCD_RS BIT0     // Register Select
+#define LCD_RW BIT1     // Read/Write
+#define LCD_E  BIT2     // Enable
+#define LCD_DATA P2OUT  // Data bus on Port 1
+char message[] = "LOCKED                          ";    // 33 characters long, 16 first row, 16 top row, \0
+void delay(unsigned int count);
+void lcd_enable_pulse();
+void lcd_write_command(unsigned char cmd);
+void lcd_write_data(unsigned char data);
+void lcd_init();
+void lcd_set_cursor(unsigned char address);
+void lcd_display_string(char *str);
+void lcd_display_message(char *str);
+void lcd_clear();
 
 //-- STATE MACHINE
 
@@ -68,6 +82,7 @@ int main(void) {
     //-- PELTIER GPIO CONTROL
 
     //-- LCD
+    lcd_init();            
 
     //-- STATE MACHINE
 
@@ -88,6 +103,7 @@ int main(void) {
         P6OUT ^= BIT6;
 
         // Delay
+        int i;
         for(i=10000; i>0; i--);
     }
     //----------------------------------------------END STATE MACHINE----------------------------------------------
@@ -100,13 +116,13 @@ void setupKeypad() {
     // columns as outputs on P4.4, P6.6, P6.5, P6.4 initialized to 0
     P4DIR |= BIT4;
     P6DIR |= BIT6 | BIT5 | BIT4;
-    P1OUT &= ~BIT4;
-    P1OUT &= ~BIT6 | ~BIT5 | ~BIT4   
+    P6OUT &= ~BIT4;
+    P6OUT &= ~(BIT6 | BIT5 | BIT4);
 
     // rows as inputs pulled down internally on P6.3, P6.2, P6.1, P6.0
-    P6DIR &= ~BIT3 | ~BIT2 | ~BIT1 | ~BIT0;     // inputs
+    P6DIR &= ~(BIT3 | BIT2 | BIT1 | BIT0);     // inputs
     P6REN |= BIT3 | BIT2 | BIT1 | BIT0;         // internal resistors
-	P2OUT &=~ BIT3 | BIT2 | BIT1 | BIT0;        // pull-downs
+	P6OUT &=~ BIT3 | BIT2 | BIT1 | BIT0;        // pull-downs
 
 }
 
@@ -195,6 +211,72 @@ int checkCols() {
 //-- PELTIER GPIO CONTROL
 
 //-- LCD
+void delay(unsigned int count) {
+    while(count--) __delay_cycles(1000);
+}
+
+void lcd_enable_pulse() {
+    P3OUT |= LCD_E;
+    delay(1);
+    P3OUT &= ~LCD_E;
+}
+
+void lcd_write_command(unsigned char cmd) {
+    P3OUT &= ~LCD_RS;  // RS = 0 for command
+    P3OUT &= ~LCD_RW;  // RW = 0 for write
+    LCD_DATA = cmd;    // Write command to data bus
+    lcd_enable_pulse();
+    delay(1);         // Command execution delay
+}
+
+void lcd_write_data(unsigned char data) {
+    P3OUT |= LCD_RS;   // RS = 1 for data
+    P3OUT &= ~LCD_RW;  // RW = 0 for write
+    LCD_DATA = data;   // Write data to data bus
+    lcd_enable_pulse();
+    delay(1);         // Data write delay
+}
+
+void lcd_init() {
+    P3DIR |= LCD_RS | LCD_RW | LCD_E;
+    P2DIR |= 0xFF;   // Set Port 1 as output for data bus
+
+    delay(50);    // Power-on delay
+
+    lcd_write_command(0x38); // Function set: 8-bit, 2 lines, 5x8 dots
+    lcd_write_command(0x0C); // Display ON, Cursor OFF
+    lcd_clear();
+    lcd_write_command(0x06); // Entry mode set: Increment cursor
+}
+
+void lcd_set_cursor(unsigned char address) {
+    lcd_write_command(0x80 | address);
+    delay(1);
+}
+
+void lcd_display_string(char *str) {
+    while(*str) {
+        lcd_write_data(*str++);
+    }
+}
+
+void lcd_clear(){
+    lcd_write_command(0x01); // Clear display
+    delay(1);             // Delay for clear command
+}
+
+void lcd_display_message(char *str ){ //Untested
+    int i;
+    lcd_set_cursor(0x00);
+    for (i=0; i<16; i++){
+        lcd_write_data(*str++);
+    }
+
+    lcd_set_cursor(0x40);
+    for (i=0; i<16; i++){
+        lcd_write_data(*str++);
+    }
+}
 
 //-- STATE MACHINE
 
